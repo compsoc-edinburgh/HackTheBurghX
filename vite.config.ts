3fd58d05c942f4da5dc71880e5ecfc5cf20f1eba
Morgan Daniel! <morganjdaniel@gmail.com>
2024-01-25 18:40:24 +0000

    yet another test :)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.obj', '**/*.mtl'],
+  assetsInclude: ['**/*.glb'], // Add this line to include glb files
   build: {
     outDir: 'docs', // Specify the custom output directory here
   },
