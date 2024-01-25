4da42fd84d1eff964bdec78f3ba472272428c39a
Morgan Daniel! <morganjdaniel@gmail.com>
2024-01-25 09:07:13 +0000

    test auto build

--- src/components/scene/Scene.tsx
+++ src/components/scene/Scene.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+// @ts-ignore
 import computer from '../../assets/computer.glb';
 import './Scene.scss';
 
